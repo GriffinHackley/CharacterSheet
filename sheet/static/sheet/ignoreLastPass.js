31f30f1f70ffca2cdb12978bf028cfb02d6bc5df
Griffin Hackley <griffinhackley@yahoo.com>
2022-11-10 12:07:03 -0700

    ignore lastpass and fixed shared localstorage bug

---
+++ sheet/static/sheet/ignoreLastPass.js
@@ -0,0 +1,6 @@
+var allInputs = document.getElementsByTagName("input")
+var allInputs = Array.prototype.slice.call( allInputs, 0 );
+
+allInputs.forEach(element => {
+    element.setAttribute("data-lpignore", true)
+});
--- sheet/static/sheet/localState.js
+++ sheet/static/sheet/localState.js
@@ -1,10 +1,11 @@
-function storeItem(key){
-    console.log(key)
-    localStorage.setItem(key, document.getElementById(key).value);
-    console.log("Storing " + key + " as " + document.getElementById(key).value)
+function storeItem(key, name) {
+    storageKey = name + "-" + key
+    localStorage.setItem(storageKey, document.getElementById(key).value);
+    console.log("Storing " + storageKey + " as " + document.getElementById(key).value)
 }
   
-function getItem(key){
-    document.getElementById(key).value = localStorage.getItem(key);
-    console.log("Getting " + key + " as " + document.getElementById(key).value)
+function getItem(key, name) {
+    storageKey = name + "-" + key
+    document.getElementById(key).value = localStorage.getItem(storageKey);
+    console.log("Getting " + storageKey + " as " + document.getElementById(key).value)
 }
