4779703daf2e5465fe2a37f836232c4a9e6c486f
Griffin Hackley <griffinhackley@yahoo.com>
2022-12-05 08:39:48 -0700

    Added init and run .bat files to make it easy for anyone to get set up and running

---
+++ init.bat
@@ -0,0 +1,5 @@
+python -m venv sheet_venv
+
+call .\sheet_venv\Scripts\activate
+
+pip install -r requirements.txt
---
+++ run.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+call .\sheet_venv\Scripts\activate
+
+python manage.py runserver
