c5647734a68b1efa6e5cc7eae4fbe15c15f5b223
Griffin Hackley <griffinhackley@yahoo.com>
2023-06-02 12:02:00 -0600

    React Init

--- CharacterSheet/urls.py
+++ CharacterSheet/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('', include('sheet.urls')),
     path('admin/', admin.site.urls),
+    path('react/', views.index, name="react")
 ]
