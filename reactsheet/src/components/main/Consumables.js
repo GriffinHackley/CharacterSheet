35d85debf372731bd1bdb81ed665de5a07f4a07a
Griffin Hackley <griffinhackley@yahoo.com>
2023-07-05 17:37:25 -0600

    Fixed consumables css

--- reactsheet/src/components/main/Consumables.js
+++ reactsheet/src/components/main/Consumables.js
@@ -1,33 +1,30 @@
-export default function Consumables({consumableInfo}){
-    let consumables = []
+import "../../css/main/Consumables.css";
 
-    consumableInfo.forEach(element => {
-        consumables.push(
-            <div key={"consumable-"+element.name}>
-                <div className="total">
-                  <div className="key">
-                    Total
-                  </div>
-                  <div className="value">
-                    { element.number }
-                  </div>
-                </div>
-                <div className="remainingConsumable">
-                  <input name="remainingConsumable"
-                         type="text"
-                         id="{{ key }}"
-                         onChange="storeItem('{{ key }}', '{{ character.name }}')"/>
-                  
-                  <label for="remainingConsumable">
-                    { element.name }
-                  </label>
-                </div>
-            </div>
-        )
-    });
-    return(
-        <div>
-            {consumables}
+export default function Consumables({ consumableInfo }) {
+  let consumables = [];
+
+  consumableInfo.forEach(element => {
+    consumables.push(
+      <div key={"consumable-" + element.name}>
+        <div className="total">
+          <div className="key">Total</div>
+          <div className="value">
+            {element.number}
+          </div>
         </div>
-    )
+        <div className="remainingConsumable">
+          <input name="remainingConsumable" type="text" id="{{ key }}" />
+
+          <label for="remainingConsumable">
+            {element.name}
+          </label>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <section className="consumables">
+      {consumables}
+    </section>
+  );
 }
